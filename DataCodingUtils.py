705b28a287802fa9e0a65333b20ec7c78dbd0776
kheffah <mtageld@emory.edu>
2021-08-12 11:12:57 -0500

    better order

--- DataCodingUtils.py
+++ DataCodingUtils.py
@@ -3,43 +3,6 @@ import numpy as np
 from typing import Union, Iterable
 
 
-def prep_data_for_conditional_survival(
-    source_table, source_table_survival_column, time_passed
-):
-    """
-    Calculates a cox regression conditional on the patients having survived to
-    a certain point in time.
-
-    Arguments
-    ---------
-    source_table: pd.DataFrame
-        pandas DF containing time to event, event, and variables as columns
-        NOTE: all variables should be either continuous or dummy variables
-    source_table_survival_column: str
-        name of time to event column
-    time_passed: Union[int,float]
-        time that has passed since start of study period
-
-    Returns
-    -------
-        a slice from the dataframe (copy)
-    """
-
-    # isolate patients that remain at-risk up to this point (survived so far)
-    # NOTE: we use .copy() to make sure we don't modify the original dataframe
-    source_table_slice = source_table.loc[
-        source_table[source_table_survival_column] >= time_passed, :
-    ].copy()
-    # reset time to zero
-    source_table_slice.loc[:, source_table_survival_column] = (
-        source_table_slice.loc[:, source_table_survival_column] - time_passed
-    )
-    # clean up
-    source_table_slice = source_table_slice.dropna()
-
-    return source_table_slice
-
-
 def get_dummies_with_nan_preservation(
         df, categorical_columns=None, verbose=False
 ):
@@ -102,3 +65,40 @@ def get_dummies_with_nan_preservation(
             df.drop(nan_colname, axis=1, inplace=True)
 
     return df
+
+
+def prep_data_for_conditional_survival(
+    source_table, source_table_survival_column, time_passed
+):
+    """
+    Calculates a cox regression conditional on the patients having survived to
+    a certain point in time.
+
+    Arguments
+    ---------
+    source_table: pd.DataFrame
+        pandas DF containing time to event, event, and variables as columns
+        NOTE: all variables should be either continuous or dummy variables
+    source_table_survival_column: str
+        name of time to event column
+    time_passed: Union[int,float]
+        time that has passed since start of study period
+
+    Returns
+    -------
+        a slice from the dataframe (copy)
+    """
+
+    # isolate patients that remain at-risk up to this point (survived so far)
+    # NOTE: we use .copy() to make sure we don't modify the original dataframe
+    source_table_slice = source_table.loc[
+        source_table[source_table_survival_column] >= time_passed, :
+    ].copy()
+    # reset time to zero
+    source_table_slice.loc[:, source_table_survival_column] = (
+        source_table_slice.loc[:, source_table_survival_column] - time_passed
+    )
+    # clean up
+    source_table_slice = source_table_slice.dropna()
+
+    return source_table_slice
